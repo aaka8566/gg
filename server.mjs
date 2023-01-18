6530a6e2455361d3c19be18104d6b877c5a8a126
aaka8566 <aakash8566@gmail.com>
2023-01-18 18:14:23 +0530

    h

--- server.mjs
+++ server.mjs
@@ -1,6 +1,10 @@
 import express from "express"
 const app = express()
 const port =process.env.PORT|| 3000;
+var cors = require('cors');
+// 
+app.use(cors());
+
 const dataa={
   "posts": [
     { "id": 1, "title": "json-server", "author": "typicode" }
