585e7e2f5ea0fcecea36c5271b40f61be5f8b6a8
aaka8566 <aakash8566@gmail.com>
2023-01-18 17:44:08 +0530

    h

--- server.mjs
+++ server.mjs
@@ -1,8 +1,19 @@
 import express from "express"
 const app = express()
 const port =process.env.PORT|| 3000;
-
+const dataa={
+  "posts": [
+    { "id": 1, "title": "json-server", "author": "typicode" }
+  ],
+  "comments": [
+    { "id": 1, "body": "some comment", "postId": 1 }
+  ],
+  "profile": { "name": "typicode" }
+}
 app.get('/', (req, res) => {
+  res.send(dataa)
+});
+app.get('/tar', (req, res) => {
   res.send('Hello World!')
 })
 
