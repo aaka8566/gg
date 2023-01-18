4815a712df96719f33e354eab207609be9caf4bf
aaka8566 <aakash8566@gmail.com>
2023-01-19 00:22:47 +0530

    h

--- server.mjs
+++ server.mjs
@@ -7,7 +7,9 @@ app.use(express.json());
 app.use(cors());
 const posts={
   "users": [
-    { "id": 1, "name": "Aakash", "email": "aakash8566@gmail.com","password":"aaki","token":"2287" }
+    { "id": 1, "name": "Aakash", "email": "aakash8566@gmail.com","password":"aaki","token":"2287" },
+    { "id": 2, "name": "Rajendra", "email": "rajendra8566@gmail.com","password":"rj","token":"3333" }
+
   ],
 }
 const dataa={
@@ -33,7 +35,21 @@ app.post('/postings', (req, res) => {
   posts.postings.push(dat);
   res.send(posts)
 });
-
+//check and login
+app.post('/users', (req, res) => {
+  const dataa=req.body;
+  
+  const found=false;
+  posts.map((el)=>{
+if(el.email===dataa.email && el.password===dataa.password){
+const found=el;
+res.send(found);
+}
+  })
+  return res.send("no user found")
+  
+});
+//check and login
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
