26b722d420d5d25f1ac02fafe95d9aec4d4627c6
aaka8566 <aakash8566@gmail.com>
2023-01-19 14:57:54 +0530

    h

--- server.mjs
+++ server.mjs
@@ -2,9 +2,10 @@ import express from "express";
 import cors from "cors";
 const app = express()
 const port =process.env.PORT|| 8000;
-app.use(express.json());
+
 // 
 app.use(cors());
+app.use(express.json());
 const posts={
   "users": [
     { "id": 1, "name": "Aakash", "email": "aakash8566@gmail.com","password":"aaki","token":"2287" },
